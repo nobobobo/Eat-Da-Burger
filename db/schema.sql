31fca7c40908d84b52f4a04b92f39ad4811a21d5
nobobobo <ianhsu1221@gmail.com>
2020-06-25 23:13:41 -0700

    sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,9 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
-CREATE TABLE burgers (
+CREATE TABLE burgers_db.burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(30),
-    devoured BOOLEAN
-)
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured) 
+VALUES
+("Double Cheese Burger",1),
+("Veggie Burger",0),
+("Turkey Burger",0);
