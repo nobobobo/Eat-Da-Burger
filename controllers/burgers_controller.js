84c370797764d6367b1cf829b1964ddd4f3e98ce
nobobobo <ianhsu1221@gmail.com>
2020-06-26 22:32:47 -0700

    controller set up

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const burger = require("../models/burger.js")
+
+const router = express.router();
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      const hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burger.insert([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.sleepy
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
+  
