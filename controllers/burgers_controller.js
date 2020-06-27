80e9587456c180802c9a7fac084bfe666ab4bad0
nobobobo <ianhsu1221@gmail.com>
2020-06-26 23:09:49 -0700

    html,css styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const burger = require("../models/burger.js")
 
-const router = express.router();
+const router = express.Router();
 
 router.get("/", function(req, res) {
     burger.all(function(data) {
--- models/burger.js
+++ models/burger.js
@@ -3,18 +3,18 @@ const orm = require("../config/orm.js");
 
 const burger = {
   all: function (cb) {
-    orm.selectAll("cats", function (res) {
+    orm.selectAll("burgers", function (res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   insert: function (cols, vals, cb) {
-    orm.insertOne("cats", cols, vals, function (res) {
+    orm.insertOne("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
   update: function (objColVals, condition, cb) {
-    orm.updateOne("cats", objColVals, condition, function (res) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
       cb(res);
     });
   }
